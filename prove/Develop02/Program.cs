a7dc9c7a96c65088656b6388457428f323c041ac
Ashish Jain <ashishjain99@yahoo.com>
2024-01-16 13:06:21 -0700

    Prove 2 and 3 updated

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,62 @@
 using System;
 
-class Program
+public class assignment_weekone_two
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+    {   
+        Console.Write("Please enter the grade percentage for letter grade: ");
+        string num = Console.ReadLine();
+        float percentage = float.Parse(num);
+
+
+        // if elif ladder:-
+        if (percentage >= 97 && percentage <= 100)
+        {
+            Console.WriteLine("Your Letter Grade is 'A+'");
+        }
+        else if (percentage >= 93 && percentage <= 96)
+        {
+            Console.WriteLine("Your Letter Grade is 'A'");
+        }
+        else if (percentage >= 90 && percentage <= 92)
+        {
+            Console.WriteLine("Your Letter Grade is 'A-'");
+        }
+        else if (percentage >= 87 && percentage <= 89)
+        {
+            Console.WriteLine("Your Letter Grade is 'B+'");
+        }
+        else if (percentage >= 83 && percentage <= 86)
+        {
+            Console.WriteLine("Your Letter Grade is B");
+        }
+        else if (percentage >= 80 && percentage <= 82)
+        {
+            Console.WriteLine("Your Letter Grade is B-");
+        }
+        else if (percentage >= 77 && percentage <= 79)
+        {
+            Console.WriteLine("Your Letter Grade is C+");
+        }
+        else if (percentage >= 73  && percentage <= 76)
+        {
+            Console.WriteLine("Your Letter Grade is C");
+        }
+        else if (percentage >= 70 && percentage <= 72)
+        {
+            Console.WriteLine("Your Letter Grade is C-");
+        }
+        else if (percentage >= 67 && percentage <= 69)
+        {
+            Console.WriteLine("Your Letter Grade is D+");
+        }
+        else if (percentage >= 65 && percentage <= 66)
+        {
+            Console.WriteLine("Your Letter Grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your Letter Grade is F");
+        }
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
-class Program
+public class assignment_weektwo_one
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Random rnd = new Random();
+        int magicNum = rnd.Next(101);
+        Console.WriteLine(magicNum);
+        bool loopVal = true;
+        int counter = 0;
+        while(loopVal)
+        {
+            Console.Write("What is your guess? ");
+            string tempGuess = Console.ReadLine();
+            int guessNum = int.Parse(tempGuess);
+            counter++;
+            if(guessNum == magicNum)
+            {
+                loopVal = false;
+                Console.WriteLine("You Guessed It!");
+                Console.WriteLine($"It took you {counter} guesses to get the write answer!");
+            }
+            else if(guessNum < magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
     }
 }
