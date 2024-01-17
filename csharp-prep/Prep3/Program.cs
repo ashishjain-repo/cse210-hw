404e5788cfd83422df9c918eeabc6e0b373ea9b3
Ashish Jain <ashishjain99@yahoo.com>
2024-01-17 14:17:52 -0700

    modified the previous code and added the function to make random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,10 +1,16 @@
 using System;
-public class assignment_weektwo_one
+public class prep3
 {
-    static void Main(string[] args)
+    public int createRandom()
     {
         Random rnd = new Random();
         int magicNum = rnd.Next(101);
+        return magicNum;
+    }
+    static void Main(string[] args)
+    {
+        prep3 prep = new prep3();
+        int magicNum = prep.createRandom();
         Console.WriteLine(magicNum);
         bool loopVal = true;
         int counter = 0;
@@ -16,9 +22,20 @@ public class assignment_weektwo_one
             counter++;
             if(guessNum == magicNum)
             {
-                loopVal = false;
                 Console.WriteLine("You Guessed It!");
                 Console.WriteLine($"It took you {counter} guesses to get the write answer!");
+                Console.WriteLine("Do you want to play again. [Y/N]");
+                char question = char.Parse(Console.ReadLine());
+                if (question == 'Y' || question == 'y')
+                {
+                    loopVal = true;
+                    magicNum = prep.createRandom();
+                    Console.WriteLine($"{magicNum}");
+                }
+                else
+                {
+                    loopVal = false;
+                }
             }
             else if(guessNum < magicNum)
             {
