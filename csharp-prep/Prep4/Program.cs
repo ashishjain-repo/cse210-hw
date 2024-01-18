535d8534fe685342fe2d7e4a79a030dd6f3a534c
Ashish Jain <ashishjain99@yahoo.com>
2024-01-18 09:33:31 -0700

    updated code for prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        bool whileLoop = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        while (whileLoop)
+        {
+            Console.Write("Enter Number: ");
+            string tempNumber = Console.ReadLine();
+            int tempInteger = int.Parse(tempNumber);
+            if (tempInteger == 0)
+            {
+                whileLoop = false;
+                int listSize = numbers.Count;
+                int listSum = 0;
+                float listAverage = 0;
+                int listLargest = numbers[0];
+                for(int i = 0; i < listSize; i++)
+                {
+                    listSum += numbers[i];
+                    if(numbers[i] > listLargest)
+                    {
+                        listLargest = numbers[i];
+                    }
+                }
+                listAverage = listSum / listSize;
+                Console.WriteLine($"The sum is: {listSum}");
+                Console.WriteLine($"The average is: {listAverage}");
+                Console.WriteLine($"The Largest Number is: {listLargest}");
+
+            }
+            else
+            {
+                numbers.Add(tempInteger);
+            }
+        }
     }
 }
