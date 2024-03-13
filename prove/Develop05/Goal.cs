7c21507c8d52430fb76dd8a3950f3290e3ecfd4f
Ashish Jain <ashishjain99@yahoo.com>
2024-03-12 21:05:50 -0600

    Added new function in main

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,7 +1,36 @@
 class Goal
 {
-    public Goal()
+    protected int _points = 0;
+    protected string[] menuOptions = {"1. Create New Goal","2. List Goals", "3. Save Goals", "4. Load Goals", "5. Record Event", "6. Quit"};
+    protected string[] goalTypes = {"1. Simple Goals", "2. Eternal Goals","3. Checklist Goals"};
+
+        
+    public Goal(int Points)
+    {
+        Points = _points;
+    }
+    public int ShowMenu()
+    {
+        Console.WriteLine("Menu Options:");
+        for(int i = 0; i < menuOptions.Count(); i++)
+        {
+            Console.WriteLine($"\t{menuOptions[i]}");
+        }
+        int Choice = Convert.ToInt32(Console.ReadLine());
+        return Choice;
+    }
+
+    // Options Functions
+    public int NewGoal()
     {
         
+        Console.WriteLine("The Types of goals are: ");
+        for(int i = 0; i < goalTypes.Count(); i++)
+        {
+            Console.WriteLine($"\t{goalTypes[i]}");
+        }
+        int Choice = Convert.ToInt32(Console.ReadLine());
+
+        return Choice;
     }
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int Points = 0;
+        Goal goal1 = new Goal(Points);
+        int menuChoice = goal1.ShowMenu();
+        int goalType = goal1.NewGoal();
     }
 }
