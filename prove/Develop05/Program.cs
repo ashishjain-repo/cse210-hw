a80bf17ea4ac55a12471e27a87bc84e67eaac40e
Ashish Jain <ashishjain99@yahoo.com>
2024-03-13 11:30:14 -0600

    Main program some templated made

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,10 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        int Points = 0;
-        List<string> Goals = new List<string>();
-        Goal goal1 = new Goal(Points,Goals);
-        int menuChoice = goal1.ShowMenu();
-        int goalType = goal1.NewGoal();
+        bool MainLoop = true;
+        while (MainLoop)
+        {
+            List<string> Goals = new List<string>();
+            int Points = 0;
+            Goal goal1 = new Goal(Points, Goals);
+            int MenuChoice = goal1.ShowMenu();
+            switch (MenuChoice)
+            {
+                case 1:
+                    int GoalType = goal1.NewGoal();
+                    bool GoalTypeBool = true;
+                    while (GoalTypeBool)
+                    {
+                        if (GoalType == 1)
+                        {
+                            GoalTypeBool = false;
+                        }
+                        else if (GoalType == 2)
+                        {
+                            GoalTypeBool = false;
+                        }
+                        else if (GoalType == 3)
+                        {
+                            GoalTypeBool = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please Choose From The Given Options");
+                            GoalTypeBool = true;
+                        }
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    MainLoop = false;
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
     }
 }
